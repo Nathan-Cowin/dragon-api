8139feece06984f3bd17f69064d11dc2b026742e
nathan <nathancowin01@gmail.com>
2025-05-10 21:26:31 +0100

    feat|test: add Trailer Sub Category table

---
+++ tests/Feature/app/Models/TrailerSubCategoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\TrailerCategory;
+use App\Models\TrailerSubCategory;
+use Illuminate\Support\Facades\Schema;
+
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertModelExists;
+
+/** Database table */
+it('exists', function () {
+    expect(Schema::hasTable('trailer_sub_categories'))->toBeTrue();
+});
+
+/** create */
+it('can create a trailer sub category', function () {
+    /** @var TrailerSubCategory $trailerSubCategory */
+    $trailerSubCategory = TrailerSubCategory::factory()->create();
+
+    assertModelExists($trailerSubCategory);
+});
+
+/** update */
+it('can update a trailer sub category', function () {
+    /** @var TrailerSubCategory $trailerSubCategory */
+    $trailerSubCategory = TrailerSubCategory::factory()->create();
+
+    $data = TrailerSubCategory::factory()->raw();
+
+    $trailerSubCategory->update($data);
+
+    $data[$trailerSubCategory->getKeyName()] = $trailerSubCategory->getKey();
+    assertDatabaseHas($trailerSubCategory->getTable(), $data);
+});
+
+/** fillable */
+it('has fillable', function () {
+    expect((new TrailerSubCategory)->getFillable())->toBe([
+        'name',
+        'long_description',
+        'trailer_category_id',
+    ]);
+});
+
+/** category() */
+it('belongs to a category', function () {
+    /** @var TrailerSubCategory $trailerSubCategory */
+    $trailerSubCategory = TrailerSubCategory::factory()->create();
+
+    expect($trailerSubCategory->trailerCategory)->toBeInstanceOf(TrailerCategory::class);
+});
+
+/** casts() */
+it('has casts', function () {
+    expect((new TrailerSubCategory)->getCasts())->toBe([
+        'id' => 'int',
+    ]);
+});
